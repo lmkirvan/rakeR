9d8cf5e98bbb839963640efa1f635926e8f96802
Kirvan <kirvanl@wdcrerpyl03.cfpb.local>
2017-07-25 17:25:18 -0400

    composing into single function that works on a vector of text

--- R/get_candidate_phrases.R
+++ R/get_candidate_phrases.R
@@ -10,7 +10,6 @@ prep_stop_words <- function(split_words = stop_words(), split_punct = basic_punc
     stop("Please provide a vector of puncation for tokenizing or use provided punctuation")
   } 
   
-  
   words <- stringr::str_c("\\b(", split_words, ")\\b", sep = "")
   names <- c(words, split_punct)
   splits <- rep("*", length(names))
@@ -39,7 +38,7 @@ candidate_phrases <- function(x, split_words = stop_words(), split_punct = basic
 
   candidates <- purrr::map(candidates, stringr::str_split, pattern = "\\*", simplify = F)
   # the list at this point is list(document)  -> list(sentence1... sentenceN) -> list(token1...2) 
-  candidates <- at_depth(.x = candidates, .depth = 1, .f = as_vector)
+  candidates <- purrr::at_depth(.x = candidates, .depth = 1, .f = purrr::as_vector)
   # todo this next line shouldn't be necessary, but I can't figure out why the split is comming back with spaces
   candidates <- purrr::map(candidates, stringr::str_trim, side = "both")
   candidates <- purrr::map(candidates, function(x) x[nchar(x) > 1])
--- R/make_coocurence_matrix.R
+++
@@ -1,7 +0,0 @@
-examples <- readr::read_csv("Book1.csv")
-
-candidates <- candidate_phrases(examples$`What Happened`)
-
-stringr::str_c(candidates[[97]], collapse = " ")
-
-examples$`What Happened`[1]
